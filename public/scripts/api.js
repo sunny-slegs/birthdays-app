097f20d7b4c28a10747e346eadf340f1239b98a2
Sonya Slegers <sonjay08@gmail.com>
2018-06-11 19:51:57 -0500

    added aajax calls to api.js

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -2,29 +2,60 @@
 
 const api = (function () {
 
-  const create = function() {
-    
+  
+  const details = function(path) {
+    return $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: path
+    });
+  };
+  
+  const search = function(path, query) {
+    return $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: path,
+      data: query
+    });
   };
 
-  const remove = function() {
-
+  const create = function(path, obj) {
+    return $.ajax({
+      type: 'POST',
+      url: path,
+      contentType: 'application/json',
+      dataType: 'json',
+      //processData: false,
+      data: JSON.stringify(obj)
+    });
   };
 
-  const update = function() {
-
+  const update = function (path, obj) {
+    return $.ajax({
+      type: 'PUT',
+      url: path,
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify(obj)
+    });
   };
 
-  const details = function() {
-
+  const remove = function(path) {
+    return $.ajax({
+      type: 'DELETE',
+      url: path,
+      dtatType: 'json'
+    });
   };
-
-  const search = function() {
-
-  };
-
-
+ 
 
   return {
-
+    search, 
+    details,
+    create, 
+    update,
+    remove
   };
+
 }());
